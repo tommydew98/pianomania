60ddf9a2ac9db2f5cb05c6e20ca05302709b28f6
yusukeito15 <yusukeito15@gmail.com>
2017-12-01 16:09:21 -0800

    Merged Game page with selecting background page
    Signed-off-by: yusukeito15 <yusukeito15@gmail.com>

--- projectapp/components/MainGame.js
+++ projectapp/components/MainGame.js
@@ -1,113 +1,159 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Text, View, Button, TouchableHighlight, StyleSheet } from 'react-native';
+import { TouchableOpacity, ActivityIndicator, Image, Text, View, Button, TouchableHighlight, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Expo, { Audio } from 'expo';
+import { ImagePicker } from 'expo';
 
 
 
 export default class MainGame extends Component {
-	static navigationOptions = {
-		title: 'Piano'
-	};
+    static navigationOptions = {
+        title: 'Piano',
+        headerStyle: {
+            backgroundColor: '#AAA'
+        }
+    };
+
+    constructor() {
+        super();
+        this.state = {
+            image: 'http://www.amromusic.com/assets/1942/steinway-model-b-ebony-satin-grand-piano-soundboard-plate-strings-tail-inside-view.jpg'
+        }
+    }
+
+    _pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            aspect: [5, 5],
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            this.setState({
+                image: result.uri
+            });
+        }
+
+    }
 
 
-  playA() {
+    playA() {
 
-    Expo.Audio.setIsEnabledAsync(true)
-    const soundA = new Expo.Audio.Sound();
+        Expo.Audio.setIsEnabledAsync(true)
+        const soundA = new Expo.Audio.Sound();
 
-    const play_soundA = (async () => {
+        const play_soundA = (async () => {
 
-      await soundA.loadAsync(require('../assets/pianoA.mp3'));
-      await soundA.playAsync();
+            await soundA.loadAsync(require('../assets/pianoA.mp3'));
+            await soundA.playAsync();
 
-    })();
-   
-  }
-  playB() {
+        })();
 
-    Expo.Audio.setIsEnabledAsync(true)
-    const soundB = new Expo.Audio.Sound();
+    }
+    playB() {
 
-    const play_soundB = (async () => {
+        Expo.Audio.setIsEnabledAsync(true)
+        const soundB = new Expo.Audio.Sound();
 
-      await soundB.loadAsync(require('../assets/pianoB.mp3'));
-      await soundB.playAsync();
+        const play_soundB = (async () => {
 
-    })();
-   
-  }
-  playC() {
+            await soundB.loadAsync(require('../assets/pianoB.mp3'));
+            await soundB.playAsync();
 
-    Expo.Audio.setIsEnabledAsync(true)
-    const soundC = new Expo.Audio.Sound();
+        })();
 
-    const play_soundC = (async () => {
+    }
+    playC() {
 
-      await soundC.loadAsync(require('../assets/pianoC.mp3'));
-      await soundC.playAsync();
+        Expo.Audio.setIsEnabledAsync(true)
+        const soundC = new Expo.Audio.Sound();
 
-    })();
-   
-  }
-  playD() {
+        const play_soundC = (async () => {
 
-    Expo.Audio.setIsEnabledAsync(true)
-    const soundD = new Expo.Audio.Sound();
+            await soundC.loadAsync(require('../assets/pianoC.mp3'));
+            await soundC.playAsync();
 
-    const play_soundD = (async () => {
+        })();
 
-      await soundD.loadAsync(require('../assets/pianoD.mp3'));
-      await soundD.playAsync();
+    }
+    playD() {
 
-    })();
-   
-  }
-  playE() {
+        Expo.Audio.setIsEnabledAsync(true)
+        const soundD = new Expo.Audio.Sound();
 
-    Expo.Audio.setIsEnabledAsync(true)
-    const soundE = new Expo.Audio.Sound();
+        const play_soundD = (async () => {
 
-    const play_soundE = (async () => {
+            await soundD.loadAsync(require('../assets/pianoD.mp3'));
+            await soundD.playAsync();
 
-      await soundE.loadAsync(require('../assets/pianoE.mp3'));
-      await soundE.playAsync();
+        })();
 
-    })();
-   
-  }
-  playF() {
+    }
+    playE() {
 
-    Expo.Audio.setIsEnabledAsync(true)
-    const soundF = new Expo.Audio.Sound();
+        Expo.Audio.setIsEnabledAsync(true)
+        const soundE = new Expo.Audio.Sound();
 
-    const play_soundF = (async () => {
+        const play_soundE = (async () => {
 
-      await soundF.loadAsync(require('../assets/pianoF.mp3'));
-      await soundF.playAsync();
+            await soundE.loadAsync(require('../assets/pianoE.mp3'));
+            await soundE.playAsync();
 
-    })();
-   
-  }
-  playG() {
+        })();
 
-    Expo.Audio.setIsEnabledAsync(true)
-    const soundG = new Expo.Audio.Sound();
+    }
+    playF() {
 
-    const play_soundG = (async () => {
+        Expo.Audio.setIsEnabledAsync(true)
+        const soundF = new Expo.Audio.Sound();
 
-      await soundG.loadAsync(require('../assets/pianoG.mp3'));
-      await soundG.playAsync();
+        const play_soundF = (async () => {
 
-    })();
-   
-  }
+            await soundF.loadAsync(require('../assets/pianoF.mp3'));
+            await soundF.playAsync();
+
+        })();
+
+    }
+    playG() {
+
+        Expo.Audio.setIsEnabledAsync(true)
+        const soundG = new Expo.Audio.Sound();
+
+        const play_soundG = (async () => {
+
+            await soundG.loadAsync(require('../assets/pianoG.mp3'));
+            await soundG.playAsync();
+
+        })();
+
+    }
 
 
-	render() {
-		return(
+    render() {
 
-			<View>
+        let {image} = this.state;
+
+        return (
+            <View>
+            <Icon.Button onPress={this._pickImage}
+            style={styles.button}
+            name="picture-o"
+            backgroundColor="#111">
+                </Icon.Button>
+
+
+            {!image && <ActivityIndicator />}
+            {image &&
+            <Image source={{
+                uri: image
+            }} style={{
+                width: 420,
+                height: 440
+            }} />}
+
 				<TouchableHighlight style={styles.keyC} onPress={this.playA}>
 					<View></View>
 				</TouchableHighlight>
@@ -130,79 +176,91 @@ export default class MainGame extends Component {
 					<View></View>
 				</TouchableHighlight>
 			</View>
-		);
-	}
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-	keyC: {
-		width: 60,
-		height: 200,
-		top: 480,
-		position: 'absolute', 
-		backgroundColor: '#fff',
-		borderColor: '#000',
-		borderWidth: 1,
-	},
-	keyD: {
-		width: 60,
-		height: 200,
-		top: 480,
-		left: 60,
-		position: 'absolute',
-		backgroundColor: '#fff',
-		borderColor: '#000',
-		borderWidth: 1,
-	},
-	keyE: {
-		width: 60,
-		height: 200,
-		top: 480,
-		left: 120,
-		position: 'absolute',
-		backgroundColor: '#fff',
-		borderColor: '#000',
-		borderWidth: 1,
-	},
-	keyF: {
-		width: 60,
-		height: 200,
-		top: 480,
-		left: 180,
-		position: 'absolute',
-		backgroundColor: '#fff',
-		borderColor: '#000',
-		borderWidth: 1,
-	},
-	keyG: {
-		width: 60,
-		height: 200,
-		top: 480,
-		left: 240,
-		position: 'absolute',
-		backgroundColor: '#fff',
-		borderColor: '#000',
-		borderWidth: 1,
-	},
-	keyA: {
-		width: 60,
-		height: 200,
-		top: 480,
-		left: 300,
-		position: 'absolute',
-		backgroundColor: '#fff',
-		borderColor: '#000',
-		borderWidth: 1,
-	},
-	keyB: {
-		width: 60,
-		height: 200,
-		top: 480,
-		left: 360,
-		position: 'absolute',
-		backgroundColor: '#fff',
-		borderColor: '#000',
-		borderWidth: 1,
-	},
+    keyC: {
+        width: 60,
+        height: 200,
+        top: 480,
+        position: 'absolute',
+        backgroundColor: '#fff',
+        borderColor: '#000',
+        borderWidth: 1,
+    },
+    keyD: {
+        width: 60,
+        height: 200,
+        top: 480,
+        left: 60,
+        position: 'absolute',
+        backgroundColor: '#fff',
+        borderColor: '#000',
+        borderWidth: 1,
+    },
+    keyE: {
+        width: 60,
+        height: 200,
+        top: 480,
+        left: 120,
+        position: 'absolute',
+        backgroundColor: '#fff',
+        borderColor: '#000',
+        borderWidth: 1,
+    },
+    keyF: {
+        width: 60,
+        height: 200,
+        top: 480,
+        left: 180,
+        position: 'absolute',
+        backgroundColor: '#fff',
+        borderColor: '#000',
+        borderWidth: 1,
+    },
+    keyG: {
+        width: 60,
+        height: 200,
+        top: 480,
+        left: 240,
+        position: 'absolute',
+        backgroundColor: '#fff',
+        borderColor: '#000',
+        borderWidth: 1,
+    },
+    keyA: {
+        width: 60,
+        height: 200,
+        top: 480,
+        left: 300,
+        position: 'absolute',
+        backgroundColor: '#fff',
+        borderColor: '#000',
+        borderWidth: 1,
+    },
+    keyB: {
+        width: 60,
+        height: 200,
+        top: 480,
+        left: 360,
+        position: 'absolute',
+        backgroundColor: '#fff',
+        borderColor: '#000',
+        borderWidth: 1,
+    },
+    container: {
+        right: 10,
+        top: 10,
+        position: "absolute",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    button: {
+        width: 50,
+        height: 50
+    },
+
 
 });
